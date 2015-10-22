30288513798c84dbef4f7f3a5e9da8f4e81a6b99
Rafa Ramírez <raffe90@gmail.com>
2015-10-22 16:21:23 +0200

    Fixes something.. I guess

--- config/dompdf.php
+++ config/dompdf.php
@@ -38,7 +38,7 @@ return array(
          * Times-Roman, Times-Bold, Times-BoldItalic, Times-Italic,
          * Symbol, ZapfDingbats.
          */
-        "DOMPDF_FONT_DIR" => public_path('/fonts/'), // advised by dompdf (https://github.com/dompdf/dompdf/pull/782)
+        "DOMPDF_FONT_DIR" => storage_path('fonts/'), // advised by dompdf (https://github.com/dompdf/dompdf/pull/782)
 
         /**
          * The location of the DOMPDF font cache directory
@@ -117,7 +117,7 @@ return array(
          * @link http://www.ros.co.nz/pdf
          * @link http://www.php.net/image
          */
-        "DOMPDF_PDF_BACKEND" => "CPDF",
+        "DOMPDF_PDF_BACKEND" => "auto",
 
         /**
          * PDFlib license key
@@ -143,7 +143,7 @@ return array(
          * the desired content might be different (e.g. screen or projection view of html file).
          * Therefore allow specification of content here.
          */
-        "DOMPDF_DEFAULT_MEDIA_TYPE" => "screen",
+        "DOMPDF_DEFAULT_MEDIA_TYPE" => "print",
 
         /**
          * The default paper size.
---
+++ storage/fonts/dompdf_font_family_cache.php
@@ -0,0 +1,132 @@
+<?php return array (
+  'sans-serif' => 
+  array (
+    'normal' => DOMPDF_DIR . '/lib/fonts/Helvetica',
+    'bold' => DOMPDF_DIR . '/lib/fonts/Helvetica-Bold',
+    'italic' => DOMPDF_DIR . '/lib/fonts/Helvetica-Oblique',
+    'bold_italic' => DOMPDF_DIR . '/lib/fonts/Helvetica-BoldOblique',
+  ),
+  'times' => 
+  array (
+    'normal' => DOMPDF_DIR . '/lib/fonts/Times-Roman',
+    'bold' => DOMPDF_DIR . '/lib/fonts/Times-Bold',
+    'italic' => DOMPDF_DIR . '/lib/fonts/Times-Italic',
+    'bold_italic' => DOMPDF_DIR . '/lib/fonts/Times-BoldItalic',
+  ),
+  'times-roman' => 
+  array (
+    'normal' => DOMPDF_DIR . '/lib/fonts/Times-Roman',
+    'bold' => DOMPDF_DIR . '/lib/fonts/Times-Bold',
+    'italic' => DOMPDF_DIR . '/lib/fonts/Times-Italic',
+    'bold_italic' => DOMPDF_DIR . '/lib/fonts/Times-BoldItalic',
+  ),
+  'courier' => 
+  array (
+    'normal' => DOMPDF_DIR . '/lib/fonts/Courier',
+    'bold' => DOMPDF_DIR . '/lib/fonts/Courier-Bold',
+    'italic' => DOMPDF_DIR . '/lib/fonts/Courier-Oblique',
+    'bold_italic' => DOMPDF_DIR . '/lib/fonts/Courier-BoldOblique',
+  ),
+  'helvetica' => 
+  array (
+    'normal' => DOMPDF_DIR . '/lib/fonts/Helvetica',
+    'bold' => DOMPDF_DIR . '/lib/fonts/Helvetica-Bold',
+    'italic' => DOMPDF_DIR . '/lib/fonts/Helvetica-Oblique',
+    'bold_italic' => DOMPDF_DIR . '/lib/fonts/Helvetica-BoldOblique',
+  ),
+  'zapfdingbats' => 
+  array (
+    'normal' => DOMPDF_DIR . '/lib/fonts/ZapfDingbats',
+    'bold' => DOMPDF_DIR . '/lib/fonts/ZapfDingbats',
+    'italic' => DOMPDF_DIR . '/lib/fonts/ZapfDingbats',
+    'bold_italic' => DOMPDF_DIR . '/lib/fonts/ZapfDingbats',
+  ),
+  'symbol' => 
+  array (
+    'normal' => DOMPDF_DIR . '/lib/fonts/Symbol',
+    'bold' => DOMPDF_DIR . '/lib/fonts/Symbol',
+    'italic' => DOMPDF_DIR . '/lib/fonts/Symbol',
+    'bold_italic' => DOMPDF_DIR . '/lib/fonts/Symbol',
+  ),
+  'serif' => 
+  array (
+    'normal' => DOMPDF_DIR . '/lib/fonts/Times-Roman',
+    'bold' => DOMPDF_DIR . '/lib/fonts/Times-Bold',
+    'italic' => DOMPDF_DIR . '/lib/fonts/Times-Italic',
+    'bold_italic' => DOMPDF_DIR . '/lib/fonts/Times-BoldItalic',
+  ),
+  'monospace' => 
+  array (
+    'normal' => DOMPDF_DIR . '/lib/fonts/Courier',
+    'bold' => DOMPDF_DIR . '/lib/fonts/Courier-Bold',
+    'italic' => DOMPDF_DIR . '/lib/fonts/Courier-Oblique',
+    'bold_italic' => DOMPDF_DIR . '/lib/fonts/Courier-BoldOblique',
+  ),
+  'fixed' => 
+  array (
+    'normal' => DOMPDF_DIR . '/lib/fonts/Courier',
+    'bold' => DOMPDF_DIR . '/lib/fonts/Courier-Bold',
+    'italic' => DOMPDF_DIR . '/lib/fonts/Courier-Oblique',
+    'bold_italic' => DOMPDF_DIR . '/lib/fonts/Courier-BoldOblique',
+  ),
+  'dejavu sans' => 
+  array (
+    'bold' => DOMPDF_DIR . '/lib/fonts/DejaVuSans-Bold',
+    'bold_italic' => DOMPDF_DIR . '/lib/fonts/DejaVuSans-BoldOblique',
+    'italic' => DOMPDF_DIR . '/lib/fonts/DejaVuSans-Oblique',
+    'normal' => DOMPDF_DIR . '/lib/fonts/DejaVuSans',
+  ),
+  'dejavu sans light' => 
+  array (
+    'normal' => DOMPDF_DIR . '/lib/fonts/DejaVuSans-ExtraLight',
+  ),
+  'dejavu sans condensed' => 
+  array (
+    'bold' => DOMPDF_DIR . '/lib/fonts/DejaVuSansCondensed-Bold',
+    'bold_italic' => DOMPDF_DIR . '/lib/fonts/DejaVuSansCondensed-BoldOblique',
+    'italic' => DOMPDF_DIR . '/lib/fonts/DejaVuSansCondensed-Oblique',
+    'normal' => DOMPDF_DIR . '/lib/fonts/DejaVuSansCondensed',
+  ),
+  'dejavu sans mono' => 
+  array (
+    'bold' => DOMPDF_DIR . '/lib/fonts/DejaVuSansMono-Bold',
+    'bold_italic' => DOMPDF_DIR . '/lib/fonts/DejaVuSansMono-BoldOblique',
+    'italic' => DOMPDF_DIR . '/lib/fonts/DejaVuSansMono-Oblique',
+    'normal' => DOMPDF_DIR . '/lib/fonts/DejaVuSansMono',
+  ),
+  'dejavu serif' => 
+  array (
+    'bold' => DOMPDF_DIR . '/lib/fonts/DejaVuSerif-Bold',
+    'bold_italic' => DOMPDF_DIR . '/lib/fonts/DejaVuSerif-BoldItalic',
+    'italic' => DOMPDF_DIR . '/lib/fonts/DejaVuSerif-Italic',
+    'normal' => DOMPDF_DIR . '/lib/fonts/DejaVuSerif',
+  ),
+  'dejavu serif condensed' => 
+  array (
+    'bold' => DOMPDF_DIR . '/lib/fonts/DejaVuSerifCondensed-Bold',
+    'bold_italic' => DOMPDF_DIR . '/lib/fonts/DejaVuSerifCondensed-BoldItalic',
+    'italic' => DOMPDF_DIR . '/lib/fonts/DejaVuSerifCondensed-Italic',
+    'normal' => DOMPDF_DIR . '/lib/fonts/DejaVuSerifCondensed',
+  ),
+  'pea mystie' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'a749f3d27f33ec3bd6c442705693020f',
+  ),
+  'avenir next condensed ultra' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . '2836cd26f7825b1f61b38ae3a19ebefd',
+    'italic' => DOMPDF_FONT_DIR . 'd72a26932501a248b8e390c65b68004c',
+  ),
+  'avenir next condensed' => 
+  array (
+    'italic' => DOMPDF_FONT_DIR . '5ae968e9c232b45d49bc6c29d0107d9e',
+    'normal' => DOMPDF_FONT_DIR . '995fbda983aab65b50cc92b0c267f061',
+    'bold_italic' => DOMPDF_FONT_DIR . 'd8f712a3c62ca2ddfa30c3469309d99a',
+    'bold' => DOMPDF_FONT_DIR . 'fdddf32b444961be9d996b5f9459557e',
+  ),
+  'avenir next condensed demi' => 
+  array (
+    'italic' => DOMPDF_FONT_DIR . '8708ea807d615f2eae323c253e9b9e2d',
+    'normal' => DOMPDF_FONT_DIR . '8d20797127f623f31bd70c9dd4e85de7',
+  ),
+) ?>
