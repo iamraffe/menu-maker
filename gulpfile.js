7bf224ed404a9684a9a26d2bfb9a274a9f6c0018
Rafa Ramírez <raffe90@gmail.com>
2015-11-06 06:32:24 -0500

    New domain setup

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ elixir(function(mix) {
 
   mix.version(['public/css/all.css', 'public/css/pdf.css']);
 
-  mix.scripts(["libs/jquery.min.js", "libs/bootstrap.min.js", "libs/jquery-ui.min.js", "libs/tinymce.min.js", "libs/sweetalert.min.js"], "public/js", "resources/assets/js");
+  mix.scripts(["libs/jquery.min.js", "libs/bootstrap.min.js", "libs/jquery-ui.min.js", "libs/sweetalert.min.js", "libs/jquery.dataTables.min.js", "libs/dataTables.bootstrap.min.js"], "public/js", "resources/assets/js");
 
 
 /*    mix.less(["admin/admin.less"], "public/css/admin/all.css");
@@ -17,4 +17,20 @@ elixir(function(mix) {
     mix.copy('resources/assets/js/admin/custom.js', 'public/js/admin/custom.js');
 
     mix.copy('resources/assets/js/admin/vendor.js', 'public/js/admin/vendor.js');*/
+});
+
+var htmlmin = require('gulp-htmlmin');
+var gulp = require('gulp');
+
+gulp.task('compress', function() {
+    var opts = {
+        collapseWhitespace:    true,
+        removeAttributeQuotes: true,
+        removeComments:        true,
+        minifyJS:              true
+    };
+
+    return gulp.src('./resources/views/partials/_wine.blade.php')
+               .pipe(htmlmin(opts))
+               .pipe(gulp.dest('./resources/views/partials/_wine.blade.php'));
 });
