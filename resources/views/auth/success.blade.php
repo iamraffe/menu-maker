356c83653322c0e23fb87db116ba5d77451ecbb1
raffe90 <raffe90@gmail.com>
2015-11-10 21:30:10 +0100

    Set redirect back to the site from parse after reset

---
+++ resources/views/auth/success.blade.php
@@ -0,0 +1,24 @@
+@extends('basic-layout')
+
+@section('content')
+<div class="container-fluid">
+    <div class="container-fluid">
+    <!-- Page Heading/Breadcrumbs -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                Password reset success
+            </h1>
+        </div>
+    </div>
+    <!-- /.row -->
+    </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1">
+        <h2>Your password has been reset correctly</h2>
+        <p>Click <a href="http://menustyler.com">here</a> to go back to the site.</p>
+    </div>
+  </div>
+</div>
+@endsection
