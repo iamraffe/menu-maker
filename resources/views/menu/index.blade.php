8a6bc40814f257d095166cb46fd3bbb71daa9404
raffe90 <raffe90@gmail.com>
2015-11-10 18:22:48 +0100

    Switch from food list to mneu

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -3,11 +3,11 @@
 @section('content')
   <div class="row menus">
     <div class="col-sm-5">
+      <h2 class="animated fadeIn"><a href="{{ url('admin/menus/menu') }}">menu</a></h2>
+    </div>
+    <div class="col-sm-5">
       <h2 class="animated fadeIn"><a href="{{ url('admin/menus/wine-list') }}">wine list</a></h2>
     </div>
-        <div class="col-sm-5">
-          <h2 class="animated fadeIn"><a href="{{ url('admin/menus/food-list') }}">food list</a></h2>
-        </div>
   </div>
 @stop
 
