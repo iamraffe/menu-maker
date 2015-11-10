0aaa5af32c438365daafb49cbc32dddeeeb694e2
raffe90 <raffe90@gmail.com>
2015-11-10 21:22:43 +0100

    Working on success route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,5 @@
 <?php
-Route::get('password/reset-success', 'Auth\PasswordController@getResetSuccess');
+Route::get('password/reset/success', 'Auth\PasswordController@getResetSuccess');
 Route::get('password/reset', 'Auth\PasswordController@getReset');
 
 
