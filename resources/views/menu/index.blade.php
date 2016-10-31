72a5e6981d823ba5dd817f96bad4a70401d29e38
Rafa Ramírez <raffe90@gmail.com>
2016-10-31 21:03:52 +0100

    Working on index menu

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -1,28 +1,14 @@
 @extends('basic-layout')
 
 @section('content')
-  @if($group->account == 'bufalinadues')
-    <div class="row menus">
-      <div class="col-sm-4">
-        <h3 class="animated fadeIn {{ $group->account }}"><a href="{{ url('admin/menus/menu') }}">menu</a></h3>
-      </div>
-      <div class="col-sm-4">
-        <h3 class="animated fadeIn {{ $group->account }}"><a href="{{ url('admin/menus/lunch-menu') }}">lunch menu</a></h3>
-      </div>
-      <div class="col-sm-4">
-        <h3 class="animated fadeIn {{ $group->account }}"><a href="{{ url('admin/menus/wine-list') }}">wine list</a></h3>
-      </div>
+  <div class="row menus">
+    <div class="col-sm-5">
+      <h3 class="animated fadeIn {{ $group->account }}"><a href="{{ url('admin/menus/menu') }}">menu</a></h3>
     </div>
-  @else
-    <div class="row menus">
-      <div class="col-sm-5">
-        <h3 class="animated fadeIn {{ $group->account }}"><a href="{{ url('admin/menus/menu') }}">menu</a></h3>
-      </div>
-      <div class="col-sm-5">
-        <h3 class="animated fadeIn {{ $group->account }}"><a href="{{ url('admin/menus/wine-list') }}">wine list</a></h3>
-      </div>
+    <div class="col-sm-5">
+      <h3 class="animated fadeIn {{ $group->account }}"><a href="{{ url('admin/menus/wine-list') }}">wine list</a></h3>
     </div>
-  @end
+  </div>
 @stop
 
 @section('script')
