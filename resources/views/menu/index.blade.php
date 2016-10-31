9d256ce4b03a17d19e162ae969985704d3d46097
Rafa Ramírez <raffe90@gmail.com>
2016-10-31 21:40:21 +0100

    Fix on index menu spacing

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -3,13 +3,13 @@
 @section('content')
   @if($group->account === 'bufalinadue')
     <div class="row menus">
-      <div class="col-sm-4">
+      <div class="col-sm-3">
         <h3 class="animated fadeIn {{ $group->account }}"><a href="{{ url('admin/menus/menu') }}">menu</a></h3>
       </div>
       <div class="col-sm-4">
         <h3 class="animated fadeIn {{ $group->account }}"><a href="{{ url('admin/menus/lunch-menu') }}">lunch menu</a></h3>
       </div>
-      <div class="col-sm-4">
+      <div class="col-sm-3">
         <h3 class="animated fadeIn {{ $group->account }}"><a href="{{ url('admin/menus/wine-list') }}">wine list</a></h3>
       </div>
     </div>
