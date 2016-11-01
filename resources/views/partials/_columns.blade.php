1b6a34776b2a916456f78f0188f645bb8aed9303
Rafa Ramirez <raffe90@gmail.com>
2016-11-01 18:40:34 +0100

    Adds new restrictions for lunch menu on template

--- resources/views/partials/_columns.blade.php
+++ resources/views/partials/_columns.blade.php
@@ -1,7 +1,7 @@
          <div class="left-column column">
             <img src="/img/logo.png" alt="Logo" class="logo">
             @foreach($categories as $category)
-                @if($category->position < 4)
+                @if($category->position < 4 && $category->menu->objectId !== 'fEush7DVHv' || $category->position < 6)
                     <div class="menu-section">
                         <h2 class="category">{!! $category->name!!}</h2>
                         <div class="item-container">
@@ -19,7 +19,7 @@
         </div>
         <div class="right-column column">
             @foreach($categories as $category)
-                @if($category->position > 3)
+                @if($category->position > 3 && $category->menu->objectId !== 'fEush7DVHv' || $category->position > 5)
                     <div class="menu-section">
                         <h2 class="category">{!! $category->name!!}</h2>
                         <div class="item-container">
